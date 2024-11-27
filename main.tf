892b59234dd3ffc1a71c34be76b79f4b68acd4b4
mastercam123 <k4mbingoce@yahoo.com>
2024-11-28 00:23:22 +0100

    Commented resources for OIDC.

--- main.tf
+++ main.tf
@@ -8,96 +8,96 @@ locals {
 # Create the required resources for the GitHub Actions to assume the IAM role
 ###########
 ## Create the OIDC provider 
-resource "aws_iam_openid_connect_provider" "github_action" {
-  url             = var.url_github_oidc
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"] # thumbprint of the OIDC provider is not used anymore but still required (suggested from GitHub)
-}
+# resource "aws_iam_openid_connect_provider" "github_action" {
+#   url             = var.url_github_oidc
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"] # thumbprint of the OIDC provider is not used anymore but still required (suggested from GitHub)
+# }
 
 ## Create the IAM policy document and the IAM role
 
-data "aws_iam_policy_document" "assume_role_oidc_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "assume_role_oidc_policy" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
 
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github_action.arn]
-    }
+#     principals {
+#       type        = "Federated"
+#       identifiers = [aws_iam_openid_connect_provider.github_action.arn]
+#     }
 
-    condition {
-      test     = "StringEquals"
-      values   = ["sts.amazonaws.com"]
-      variable = "token.actions.githubusercontent.com:aud"
-    }
+#     condition {
+#       test     = "StringEquals"
+#       values   = ["sts.amazonaws.com"]
+#       variable = "token.actions.githubusercontent.com:aud"
+#     }
 
-    condition {
-      test     = "StringLike"
-      variable = "${aws_iam_openid_connect_provider.github_action.url}:sub"
-      values = [
-        local.string_development_branch,
-        local.string_production_branch,
-        local.string_tag
-      ]
-    }
-  }
-}
+#     condition {
+#       test     = "StringLike"
+#       variable = "${aws_iam_openid_connect_provider.github_action.url}:sub"
+#       values = [
+#         local.string_development_branch,
+#         local.string_production_branch,
+#         local.string_tag
+#       ]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "github_action_role" {
-  name               = var.github_action_role_name
-  assume_role_policy = data.aws_iam_policy_document.assume_role_oidc_policy.json
-}
+# resource "aws_iam_role" "github_action_role" {
+#   name               = var.github_action_role_name
+#   assume_role_policy = data.aws_iam_policy_document.assume_role_oidc_policy.json
+# }
 
 ## Fetch AWS Managed policy attach it to the role
-data "aws_iam_policy" "S3_full_access" {
-  name = "AmazonS3FullAccess"
-}
-resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-  policy_arn = data.aws_iam_policy.S3_full_access.arn
-  role       = aws_iam_role.github_action_role.name
-}
+# data "aws_iam_policy" "S3_full_access" {
+#   name = "AmazonS3FullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+#   policy_arn = data.aws_iam_policy.S3_full_access.arn
+#   role       = aws_iam_role.github_action_role.name
+# }
 ## Add additional policy that only allows access to the bucket where terraform state file lies
-resource "aws_iam_policy" "deny_outside_tf_bucket" {
-  name        = "DenyOutsideTFStateBucket"
-  description = "Deny full access actions outside the S3 bucket for terraform state file"
+# resource "aws_iam_policy" "deny_outside_tf_bucket" {
+#   name        = "DenyOutsideTFStateBucket"
+#   description = "Deny full access actions outside the S3 bucket for terraform state file"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Deny"
-        Action = "s3:*"
-        Resource = [
-          "arn:aws:s3:::*",
-          "arn:aws:s3:::*/*"
-        ]
-        Condition = {
-          StringNotEquals = {
-            "s3:prefix" = var.S3_bucket_name_state_file
-          }
-        }
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Deny"
+#         Action = "s3:*"
+#         Resource = [
+#           "arn:aws:s3:::*",
+#           "arn:aws:s3:::*/*"
+#         ]
+#         Condition = {
+#           StringNotEquals = {
+#             "s3:prefix" = var.S3_bucket_name_state_file
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "attach_deny_outside_test_bucket_policy" {
-  policy_arn = aws_iam_policy.deny_outside_tf_bucket.arn
-  role       = aws_iam_role.github_action_role.name
-}
+# resource "aws_iam_role_policy_attachment" "attach_deny_outside_test_bucket_policy" {
+#   policy_arn = aws_iam_policy.deny_outside_tf_bucket.arn
+#   role       = aws_iam_role.github_action_role.name
+# }
 
 ###########
 # Create the required resources for the tasks
 ###########
 ## Fetch AWS Managed policy for lambda attach it to the role
-data "aws_iam_policy" "lambda_full_access" {
-  name = "AWSLambda_FullAccess"
-}
-resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
-  policy_arn = data.aws_iam_policy.lambda_full_access.arn
-  role       = aws_iam_role.github_action_role.name
-}
+# data "aws_iam_policy" "lambda_full_access" {
+#   name = "AWSLambda_FullAccess"
+# }
+# resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
+#   policy_arn = data.aws_iam_policy.lambda_full_access.arn
+#   role       = aws_iam_role.github_action_role.name
+# }
 
 
 #######################################################################
