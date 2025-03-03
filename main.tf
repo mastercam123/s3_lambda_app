b7a4729565c8a9bd4ddda93e1b65d0330a879207
mastercam123 <k4mbingoce@yahoo.com>
2025-03-03 13:16:54 +0100

    Added volume to test atlantis in another AWS account.

--- main.tf
+++ main.tf
@@ -104,29 +104,19 @@
 #######################################################################
 # Create the required resources for the tasks
 #######################################################################
-## Create S3 bucket for the data
-# resource "aws_s3_bucket" "string_bucket" {
-#   bucket = local.s3_bucket_name_state_file
-# }
+resource "aws_ebs_volume" "vol_test_atlantis1" {
+  availability_zone = "eu-central-1a"
+  size              = 8
+  tags = {
+    Name = "test_vol_atlantis"
+  }
+}
 
-# ## Call module to create lambda function for reversed string
-# module "name_prefix_filter" {
-#   source         = "./tf-modules/s3-reversed"
-#   s3_bucket_name = local.s3_bucket_name_state_file
-#   input_prefix   = var.input_prefix_filter
-#   output_prefix  = var.output_prefix_filter
-#   env_prefix     = var.env_prefix
-# }
-# ## Create S3 Bucket notification for the lambda function
-# resource "aws_s3_bucket_notification" "object_put_notification" {
-#   bucket = aws_s3_bucket.string_bucket.id
-
-#   lambda_function {
-#     lambda_function_arn = module.name_prefix_filter.lambda_function_arn
-#     events              = ["s3:ObjectCreated:*"]
-#     filter_prefix       = var.input_prefix_filter
-#   }
-
-#   depends_on = [module.name_prefix_filter]
-# }
+resource "aws_ebs_volume" "vol_test_atlantis2" {
+  availability_zone = "eu-central-1a"
+  size              = 4
+  tags = {
+    Name = "test_vol_atlantis2"
+  }
+}
 
