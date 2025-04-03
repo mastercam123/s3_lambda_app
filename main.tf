47c631f2368bec642545a92f8f854f961f03e865
mastercam123 <k4mbingoce@yahoo.com>
2025-04-03 13:55:33 +0200

    Updated EBS volume.

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@
 #######################################################################
 resource "aws_ebs_volume" "vol_test_atlantis1" {
   availability_zone = "eu-central-1a"
-  size              = 4
+  size              = 10
   tags = {
     Name = "test_vol_atlantis"
   }
